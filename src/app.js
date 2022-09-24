52c81bcf27b77c03fdd789bb438e2f9bb82d9d48
Serhii Rubtsov <rubtsov1986@bigmir.net>
2022-09-24 17:41:45 +0300

    add swagger to app + add swagger.json

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
 const { authRouter } = require('./routes/api/auth');
 
 const app = express();
@@ -11,6 +14,8 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api/users', authRouter);
 
