6a25a2329d4010c9a0e5f38ebf6d083bab2edaee
puerarongo <akhardin96@gmail.com>
2022-09-25 20:08:54 +0300

    add current and logout routes

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,14 +1,8 @@
 const User = require('../models/user');
 const createError = require('http-errors');
-const axios = require('axios');
-const queryString = require('query-string'); 
-// const URL = require('url');
-require('dotenv').config();
 
 const { passwordGeneration, passwordCompare } = require("../services/passwordGeneration");
 const { tokenGeneration } = require("../services/tokenGeneration");
-
-
 
 
 const registration = async (req, res, next) => {
@@ -21,7 +15,7 @@ const registration = async (req, res, next) => {
             throw error;
         }
 
-        // hash password
+        // ? hash password
         const hashPassword = await passwordGeneration(password);
 
         const data = await User.create({ name, email, password: hashPassword });
@@ -60,8 +54,6 @@ const login = async (req, res, next) => {
         await User.findByIdAndUpdate(user._id, { token });
 
         res.status(200).json({token})
-
-
     }
     catch (err) {
         if (err.message.includes("validation failed")) err.status = 400;
@@ -70,23 +62,11 @@ const login = async (req, res, next) => {
 };
 
 
-const googleAuth = async (req, res, next) => {
+const logout = async (req, res, next) => { 
     try {
-        const stringifyParams = queryString.stringify({
-            client_id: process.env.GOOGLE_ID,
-            redirect_uri: `${process.env.BASE_URL}/api/user/google-redirect`,
-            scope: [
-                "https://www.googleapis.com/auth/userinfo.email",
-                "https://www.googleapis.com/auth/userinfo.profile",
-            ].join(" "),
-            response_type: "code",
-            access_type: "offline",
-            prompt: "consent"
-        });
-
-        return res.redirect(
-            `https://accounts.google.com/o/oauth2/v2/auth?${stringifyParams}`
-        );
+        const { _id } = req.user;
+        await User.findByIdAndUpdate(_id, { token: null });
+        res.status(204).send();
     }
     catch (err) {
         next(err);
@@ -94,54 +74,14 @@ const googleAuth = async (req, res, next) => {
 };
 
 
-const googleRedirect = async (req, res, next) => {
-    try {
-        const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
-        // console.log("fullURL", fullUrl)
-        const urlObj = new URL(fullUrl);
-        // console.log("urlOBJ", urlObj)
-
-        const urlParams = queryString.parse(urlObj.search);
-        // console.log("urlPARAMS", urlParams)
-        const code = urlParams.code;
-        // console.log("code", code)
-
-        const tokenData = await axios({
-            url: `https://oauth2.googleapis.com/token`,
-            method: "post",
-            data: {
-                client_id: process.env.GOOGLE_ID,
-                client_secret: process.env.GOOGLE_SECRET,
-                redirect_uri: `${process.env.BASE_URL}/api/user/google-redirect`,
-                grant_type: "authorization_code",
-                code: code,
-            },
-        });
-
-        const userData = await axios({
-            url: "https://www.googleapis.com/oauth2/v2/userinfo",
-            method: "get",
-            headers: {
-                Authorization: `Bearer ${tokenData.data.access_token}`,
-            },
-        });
-
-        const { email, name } = userData.data;
-        console.log(userData.data)
-        console.log(email, name)
-
-        return res.redirect(
-            `${process.env.FRONTEND_URL}?email=${userData.data.email}`
-        )
-    }
-    catch (err) {
-        next(err);
-    }
+const current = async (req, res, next) => { 
+    const { name, email } = req.user;
+    res.status(200).json({ name, email });
 };
 
 module.exports = {
     registration,
     login,
-    googleAuth,
-    googleRedirect
+    logout,
+    current
 };
