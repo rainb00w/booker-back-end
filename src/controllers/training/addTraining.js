baa80b8ede7088d21ebc19fb0c0ca09bfc27e19b
Lilia Lomaka <llomaka80@gmail.com>
2022-09-27 21:21:55 +0300

    Deleting completed training on get training request

--- src/controllers/training/addTraining.js
+++ src/controllers/training/addTraining.js
@@ -4,25 +4,28 @@ const { RequestError } = require('../../helpers')
 const addTraining = async (req, res) => {
     const { _id: owner } = req.user
     const { body } = req
-    for (const book of body.books) {
+    body.books.forEach(async (book) => {
         const originalBook = await booksServices.getById({ bookId: book._id, owner })
         if (originalBook.status === 'haveRead' || originalBook.status === 'reading') {
             throw RequestError(400, 'Chosen books have already been read.')
         }
-    }
+    })
     if (new Date(body.finishDate).getTime() - new Date(body.startDate).getTime() > (32 * 24 * 3600 * 1000) || new Date(body.finishDate).getTime() - new Date(body.startDate).getTime() < (24 * 3600 * 1000)) {
         throw RequestError(400, 'Training period should be greater than 1 day and must not exceed 31 days.')
     }
-    const currentTraining = await trainingServices.getTraining(owner)
-    if (currentTraining) {
-        const { finishDate, _id } = currentTraining
-        if (currentTraining.completed) {
-            body.books.forEach(async book => {
+    const startTraining = async () => {
+        body.books.forEach(async book => {
                 await booksServices.updateBookStatus(book._id, owner, { status: 'reading' })
             })
-            await trainingServices.deleteTraining(_id)
             await trainingServices.addTraining(owner, body)
-            res.status(201).json(await trainingServices.getTraining(owner))
+        res.status(201).json(await trainingServices.getTraining(owner))
+    }
+    const currentTraining = await trainingServices.getTraining(owner)
+    if (currentTraining) {
+        const { finishDate, completed, books: oldBooks, _id } = currentTraining
+        if (completed) {
+            await trainingServices.deleteTraining(_id)
+            startTraining()
         }
         const currentDate = new Date()
         if (finishDate.getTime() >= currentDate.getTime()) {
@@ -30,14 +33,13 @@ const addTraining = async (req, res) => {
             const totalDays = Math.ceil(difference / (1000 * 3600 * 24))
             throw RequestError(403, `Training is in progress. Try again in ${totalDays} days.`)
         } else {
+            oldBooks.forEach(async book => {
+                await booksServices.updateBookStatus(book._id, owner, { status: 'toRead' })
+            })
             await trainingServices.deleteTraining(_id)
         }
     }
-    body.books.forEach(async book => {
-        await booksServices.updateBookStatus(book._id, owner, { status: 'reading' })
-    })
-    await trainingServices.addTraining(owner, body)
-    res.status(201).json(await trainingServices.getTraining(owner))
+    startTraining()
 }
 
 module.exports = addTraining
