78af7c6d0d0897dd7c7afedee22796a370f44200
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2022-09-27 13:06:09 +0300

    _vtest

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const app = require('./src/app');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const { MAIN_PORT = 3001, DB_HOST } = process.env;
+const { PORT = 3001, DB_HOST } = process.env;
 
 mongoose.connect(DB_HOST)
-    .then(() => app.listen(MAIN_PORT, () => {
-        console.log(`Database connection successful on port ${MAIN_PORT}`)
+    .then(() => app.listen(process.env.PORT || 3001, () => {
+        console.log(`Database connection successful on port ${PORT}`)
     })
     )
     .catch(err => {
@@ -14,3 +14,14 @@ mongoose.connect(DB_HOST)
         process.exit(1)
     });
 
+
+
+    // mongoose.connect(DB_HOST)
+    // .then(() => app.listen(MAIN_PORT, () => {
+    //     console.log(`Database connection successful on port ${MAIN_PORT}`)
+    // })
+    // )
+    // .catch(err => {
+    //     console.error(err)
+    //     process.exit(1)
+    // });
